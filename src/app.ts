fb41f05567715ee85b9312c05a4e7a7a4a6feb57
Agustin Gomez <agusleongomez@gmail.com>
2023-05-16 23:43:01 -0300

    created express app

---
+++ src/app.ts
@@ -0,0 +1,7 @@
+import express, { Application } from "express";
+
+const app: Application = express();
+
+app.set("port", 4000);
+
+export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+import app from "./app";
+
+function main() {
+  app.listen(app.get("port"), () => {
+    console.log(`Server is listening on port ${app.get("port")}`);
+  });
+}
+
+main();

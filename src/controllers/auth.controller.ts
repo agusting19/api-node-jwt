4160a098ccfdd340b73943db22878c231656ab10
Agustin Gomez <agusleongomez@gmail.com>
2023-05-18 17:26:24 -0300

    created singup controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,7 +1,25 @@
 import { Request, Response } from "express";
+import { UserInterface } from "types/User.types";
+import User from "../models/User";
+import jwt from "jsonwebtoken";
 
 export const singup = async (req: Request, res: Response) => {
-  res.send("singup");
+  // Saving a new User
+  const user: UserInterface = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  user.password = await user.encryptPassword(user.password);
+  const savedUser = await user.save();
+
+  // Create a token
+  const token: string = jwt.sign(
+    { _id: savedUser._id },
+    process.env.TOKEN_SECRET || "tokentest"
+  );
+  res.header("auth-token", token).json(savedUser);
 };
 export const singin = (req: Request, res: Response) => {
   res.send("singin");
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
+import dotenv from "dotenv";
 import app from "./app";
 import "./database";
+
+dotenv.config();
 
 function main() {
   app.listen(app.get("port"), () => {

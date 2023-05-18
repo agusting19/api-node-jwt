afc3cc8c7d5fb0281b2ac81b6867220a1f647f8e
Agustin Gomez <agusleongomez@gmail.com>
2023-05-18 16:19:09 -0300

    added middleware, routes and controllers

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,17 @@
 import express, { Application } from "express";
+import authRoutes from "./routes/auth";
+import morgan from "morgan";
 
 const app: Application = express();
 
+// Settings
 app.set("port", 4000);
+
+// Middlewares
+app.use(morgan("dev"));
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
 
 export default app;
---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+
+export const singup = async (req: Request, res: Response) => {
+  res.send("singup");
+};
+export const singin = (req: Request, res: Response) => {
+  res.send("singin");
+};
+export const profile = (req: Request, res: Response) => {
+  res.send("profile");
+};

8027fad13616ce66706108b34d2145c1e5bc1abe
Agustin Gomez <agusleongomez@gmail.com>
2023-05-18 17:37:38 -0300

    created singin controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -17,13 +17,35 @@ export const singup = async (req: Request, res: Response) => {
   // Create a token
   const token: string = jwt.sign(
     { _id: savedUser._id },
-    process.env.TOKEN_SECRET || "tokentest"
+    process.env.SECRET_TOKEN || "tokentest"
   );
   res.header("auth-token", token).json(savedUser);
 };
-export const singin = (req: Request, res: Response) => {
-  res.send("singin");
+
+export const singin = async (req: Request, res: Response) => {
+  // Validate email
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) return res.status(400).json("Wrong email or password");
+  // Validate password
+  const correctPassword: boolean = await user.validatePassword(
+    req.body.password
+  );
+
+  if (!correctPassword) return res.status(400).json("Invalid password");
+
+  // Create a token
+  const token = jwt.sign(
+    { _id: user._id },
+    process.env.SECRET_TOKEN || "tokentest",
+    {
+      expiresIn: 60 * 60 * 24,
+    }
+  );
+
+  res.header("auth-token", token).json(user);
 };
+
 export const profile = (req: Request, res: Response) => {
   res.send("profile");
 };
